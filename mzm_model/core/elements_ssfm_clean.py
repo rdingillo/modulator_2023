Updated functions to retrieve powers of different MZMs inside; semi-correct power calculation (post-SOA to be checked)

mzm_model/core/elements_ssfm_clean.py
@@ -210,10 +210,14 @@ class InP_MZM(object):
         self._gamma_2 = gamma_2
         self._b = b
         self._c = c
-        self._phase_vl = 0
-        self._phase_vr = 0
-        self._transmission_vl = 0
-        self._transmission_vr = 0
+        self._vl = vcm_phase - vdiff/2
+        self._vr = vcm_phase + vdiff/2
+        self._intensities = self.griffin_intensity_tf()
+        self._phases = self.griffin_phase()
+        self._phase_vl = self._phases[0]
+        self._phase_vr = self._phases[1]
+        self._transmission_vl = self._intensities[0]
+        self._transmission_vr = self._intensities[1]
 
     @property
     def lambda_mzm(self):
@@ -230,6 +234,14 @@ class InP_MZM(object):
     @property
     def vdiff(self):
         return self._vdiff
+
+    @property
+    def vl(self):
+        return self._vl
+
+    @property
+    def vr(self):
+        return self._vr
 
     @property
     def vpi(self):
@@ -258,6 +270,14 @@ class InP_MZM(object):
     @property
     def phase_vl(self):
         return self._phase_vl
+
+    # @vl.setter
+    # def vl(self, vl):
+    #     self._vl = vl
+    #
+    # @vr.setter
+    # def vr(self, vr):
+    #     self._vr = vr
 
     @phase_vl.setter
     def phase_vl(self, phase_vl):
@@ -298,10 +318,10 @@ class InP_MZM(object):
     # define V-dependent intensity tf
     def griffin_intensity_tf(self):
         c = self.c
-        vl = self.eval_varm()[0]
-        vr = self.eval_varm()[1]
-        tf_l = (1 + np.exp((vl - c) / 0.8)) ** -1.25
-        tf_r = (1 + np.exp((vr - c) / 0.8)) ** -1.25
+        # vl = self.eval_varm()[0]
+        # vr = self.eval_varm()[1]
+        tf_l = (1 + np.exp((self.vl - c) / 0.8)) ** -1.25
+        tf_r = (1 + np.exp((self.vr - c) / 0.8)) ** -1.25
 
         return [tf_l, tf_r]
 
@@ -309,10 +329,10 @@ class InP_MZM(object):
         b = self.b
         v_pi = self.vpi
         vcm_phase = self.vcm_phase
-        vl = self.eval_varm()[0]
-        vr = self.eval_varm()[1]
-        phase_l = ((2 * b * vcm_phase * v_pi - np.pi) / v_pi) * vl - b * vl ** 2
-        phase_r = ((2 * b * vcm_phase * v_pi - np.pi) / v_pi) * vr - b * vl ** 2
+        # vl = self.eval_varm()[0]
+        # vr = self.eval_varm()[1]
+        phase_l = ((2 * b * vcm_phase * v_pi - np.pi) / v_pi) * self.vl - b * self.vl ** 2
+        phase_r = ((2 * b * vcm_phase * v_pi - np.pi) / v_pi) * self.vr - b * self.vr ** 2
 
         return [phase_l, phase_r]
 
@@ -320,24 +340,29 @@ class InP_MZM(object):
         gamma_1 = self.gamma_1
         gamma_2 = self.gamma_2
         phase_offset = self.phase_offset
-        vl = self.eval_varm()[0]
-        vr = self.eval_varm()[1]
-        transmission_vl = self.griffin_intensity_tf()[0]
-        transmission_vr = self.griffin_intensity_tf()[1]
-        phase_vl = self.griffin_phase()[0]
-        phase_vr = self.griffin_phase()[1]
-        self.transmission_vl = transmission_vl
-        self.transmission_vr = transmission_vr
-        self.phase_vl = phase_vl
-        self.phase_vr = phase_vr
+        # vl = self.vl
+        # vr = self.vr
+        # transmission_vl = self.griffin_intensity_tf()[0]
+        # transmission_vr = self.griffin_intensity_tf()[1]
+        # phase_vl = self.griffin_phase()[0]
+        # phase_vr = self.griffin_phase()[1]
+        # self.transmission_vl = transmission_vl
+        # self.transmission_vr = transmission_vr
+        # self.phase_vl = phase_vl
+        # self.phase_vr = phase_vr
         # ext_r = self.er
 
         # eo_tf_field = np.sqrt(1 - 1 / ext_r) * gamma_1 * gamma_2 * np.sqrt(transmission_vl) * np.exp(phase_vl * 1j) +
         #     np.sqrt(1 - 1 / ext_r) * np.sqrt(np.abs(1 - gamma_1 ** 2) * np.abs(1 - gamma_2 ** 2)) * np.sqrt(
         #     transmission_vr) * np.exp((phase_vr + phase_offset) * 1j)
+        transmission_vl = self.transmission_vl
+        transmission_vr = self.transmission_vr
+        phase_vl = self.phase_vl
+        phase_vr = self.phase_vr
+
         eo_tf_field = gamma_1 * gamma_2 * np.sqrt(transmission_vl) * np.exp(phase_vl * 1j) + \
                       np.sqrt(np.abs(1 - gamma_1 ** 2) * np.abs(1 - gamma_2 ** 2)) * np.sqrt(transmission_vr) * \
-                      np.exp((phase_vr + phase_offset) * 1j)
+                      np.exp(phase_vr * 1j)
         eo_field_conj = np.conjugate(eo_tf_field)
         eo_tf_power = eo_tf_field * eo_field_conj
         return eo_tf_power
@@ -346,24 +371,29 @@ class InP_MZM(object):
         gamma_1 = self.gamma_1
         gamma_2 = self.gamma_2
         phase_offset = self.phase_offset
-        vl = self.eval_varm()[0]
-        vr = self.eval_varm()[1]
-        transmission_vl = self.griffin_intensity_tf()[0]
-        transmission_vr = self.griffin_intensity_tf()[1]
-        phase_vl = self.griffin_phase()[0]
-        phase_vr = self.griffin_phase()[1]
-        self.transmission_vl = transmission_vl
-        self.transmission_vr = transmission_vr
-        self.phase_vl = phase_vl
-        self.phase_vr = phase_vr
+        # vl = self.vl
+        # vr = self.vr
+        # transmission_vl = self.griffin_intensity_tf()[0]
+        # transmission_vr = self.griffin_intensity_tf()[1]
+        # phase_vl = self.griffin_phase()[0]
+        # phase_vr = self.griffin_phase()[1]
+        # self.transmission_vl = transmission_vl
+        # self.transmission_vr = transmission_vr
+        # self.phase_vl = phase_vl
+        # self.phase_vr = phase_vr
         # ext_r = self.er
 
         # eo_tf_field = np.sqrt(1 - 1 / ext_r) * gamma_1 * gamma_2 * np.sqrt(transmission_vl) * np.exp(phase_vl * 1j) +
         #     np.sqrt(1 - 1 / ext_r) * np.sqrt(np.abs(1 - gamma_1 ** 2) * np.abs(1 - gamma_2 ** 2)) * np.sqrt(
         #     transmission_vr) * np.exp((phase_vr + phase_offset) * 1j)
+
+        transmission_vl = self.transmission_vl
+        transmission_vr = self.transmission_vr
+        phase_vl = self.phase_vl
+        phase_vr = self.phase_vr
         eo_tf_field = gamma_1 * gamma_2 * np.sqrt(transmission_vl) * np.exp(phase_vl * 1j) + \
                       np.sqrt(np.abs(1 - gamma_1 ** 2) * np.abs(1 - gamma_2 ** 2)) * np.sqrt(transmission_vr) * \
-                      np.exp((phase_vr + phase_offset) * 1j)
+                      np.exp(phase_vr * 1j)
         return eo_tf_field
 
     # def griffin_il_er(self):

mzm_model/core/science_utils_ssfm.py
@@ -4,8 +4,9 @@ import random
 from numpy import sum,isrealobj,sqrt
 from numpy.random import standard_normal
 import pandas as pd
+from mzm_model.core.math_utils import lin2dbm, dbm2lin
 
-from mzm_model.core.modulator_ssfm_params import M, L
+from mzm_model.core.modulator_ssfm_params import M, L, phi_laser, b, vcm_phase
 
 '''Modulation format methods'''
 
@@ -100,3 +101,21 @@ def awgn(s,SNRdB,L=1):
         n = sqrt(N0/2)*(standard_normal(s.shape)+1j*standard_normal(s.shape))
     r = s + n # received signal
     return r
+
+
+def evaluate_field(power):
+    out_field = np.sqrt(dbm2lin(power)) * np.exp(phi_laser * -2j)
+    return out_field
+
+
+def griffin_phase_electrode(vl, vr, vpi_l, vpi_r):
+    phase_l = ((2 * b * vcm_phase * vpi_l - np.pi) / vpi_l) * vl - b * vl ** 2
+    phase_r = ((2 * b * vcm_phase * vpi_r - np.pi) / vpi_r) * vr - b * vr ** 2
+    return [phase_l, phase_r]
+
+
+def field_to_power_dbm(field):
+    eo_field_conj = np.conjugate(field)
+    eo_tf_power = lin2dbm(field * eo_field_conj)
+    return eo_tf_power
+

mzm_model/core/soa_config_singlechannel.py
@@ -3,7 +3,7 @@ import pandas as pd
 import json
 from pathlib import Path
 from mzm_model.core.elements_ssfm_clean import SSFMLightSource
-from mzm_model.core.modulator_ssfm_params import json_spectral, frequency, current_in_pre_soa, current_in_post_soa
+from mzm_model.core.modulator_ssfm_params import json_spectral, json_soa, frequency, current_in_pre_soa, current_in_post_soa
 root = Path('~/PycharmProjects/modulator_2023/').expanduser()
 json_source = root.parent / 'optical-system-interface/resources/ssfm_test_configs/lumentum_modulator/spectral_information'
 
@@ -30,12 +30,12 @@ laser_input_power_dbm = source.input_power
 power_in = laser_input_power_dbm  # dBm, typically 13 dBm
 
 # mod_loss_set = np.arange(6, 15)                          # dB
-pbo_loss = 10  # dB, previously called modulation loss
-quad_loss = 3  # dB, quadrature loss
-loss_coupling = 3  # dB, loss of coupler
-loss_split_xy = 3  # dB, loss of polarization splitter
-loss_wg_pre_soa = 1  # dB, loss of waveguide BEFORE pre-SOA
-loss_wg_post_soa = 3  # dB, loss of waveguide BEFORE post-SOA
+pbo_loss = json_soa["pbo_loss"]  # dB, previously called modulation loss
+quad_loss = json_soa["quad_loss"]  # dB, quadrature loss
+loss_coupling = json_soa["loss_coupling"]  # dB, loss of coupler
+loss_split_xy = json_soa["loss_split_xy"]  # dB, loss of polarization splitter
+loss_wg_pre_soa = json_soa["loss_wg_pre_soa"]  # dB, loss of waveguide BEFORE pre-SOA
+loss_wg_post_soa = json_soa["loss_wg_post_soa"]  # dB, loss of waveguide BEFORE post-SOA
 # loss_wvl_dep_wg_data_post_soa = np.array(
 #     [1.7, 1.3, 1.2, 1.4, 1.9, 2.8])  # wavelength dependent losses in wg after post soa
 
@@ -112,6 +112,10 @@ beta_pre_soa = evaluate_beta(p1db_pre_soa, gs_pre_soa)
 
 p_out_pre_soa = evaluate_output_power(power_in_pre_soa, gs_pre_soa, beta_pre_soa)
 
+
+def pre_soa_out_power():
+    return p_out_pre_soa
+
 gs_post_soa = evaluate_gs(a0, a1, a2, current_in_post_soa)
 p1db_post_soa = evaluate_p1db_post(b0, b1, b2, current_in_post_soa)
 beta_post_soa = evaluate_beta(p1db_post_soa, gs_post_soa)
@@ -121,3 +125,9 @@ power_in_post_soa = p_out_pre_soa - post_soa_loss
 p_out_post_soa = evaluate_output_power(power_in_post_soa, gs_post_soa, beta_post_soa)
 osnr_db = evaluate_osnr_db(power_in_post_soa, channel)
 print()
+
+
+def post_soa_out_power(p_in_dbm):
+    power_in = p_in_dbm - post_soa_loss
+    power_out = evaluate_output_power(power_in, gs_post_soa, beta_post_soa)
+    return power_out

mzm_model/core/utils_ssfm.py
@@ -5,7 +5,7 @@ import numpy as np
 import seaborn as sns; sns.set_theme()
 
 from scipy.stats import gaussian_kde
-from mzm_model.core.modulator_ssfm_params import v_pi, er, insertion_loss, phase_offset, v_off, b, c, v_cm, \
+from mzm_model.core.modulator_ssfm_params import v_pi, er, insertion_loss, phase_offset, v_off, b, c, vcm_phase, \
     modulation_format
 from mzm_model.core.math_utils import lin2db
 

tasks/dc_mzm_models.py
@@ -9,8 +9,10 @@ from mzm_model.core.elements_ssfm_clean import SSFMLightSource, Splitter, Wavegu
 #     v_diff, v_in_limit, v_in_step, er, insertion_loss, phase_offset, v_off, v_bias, b, c, wavelength, gamma_1, gamma_2,\
 #     v_pi_values
 from mzm_model.core.modulator_ssfm_params import h, frequency, q, v_pi,phase_offset, v_off, b, c, gamma_1, gamma_2,\
-    v_pi_values
+    v_pi_values, lambda_wave, vcm_phase, vcm_bias, v_diff_i, v_diff_q, v_diff_ph
 from mzm_model.core.math_utils import lin2db, lin2dbm, db2lin, dbm2lin
+from mzm_model.core.soa_config_singlechannel import pre_soa_out_power, post_soa_out_power
+from mzm_model.core.science_utils_ssfm import evaluate_field, griffin_phase_electrode, field_to_power_dbm
 import json
 
 start_time = time.time()
@@ -36,37 +38,76 @@ source = SSFMLightSource(json_spectral)
 
 # evaluate optical input power in dBm
 input_power_dbm = source.input_power  # dBm
-"""TODO: QUI APPLICARE IL PRE-SOA POWER"""
 input_power = dbm2lin(input_power_dbm)  # Watt
 source.out_field = source.calculate_optical_input_power(input_power_dbm)
 input_field = source.out_field
 
-# define TX amplitude parameter in time (for the field)
-k_tx = (np.sqrt(input_power) / (np.sqrt(2))) * (np.pi / (2 * v_pi))
+# # define TX amplitude parameter in time (for the field)
+# k_tx = (np.sqrt(input_power) / (np.sqrt(2))) * (np.pi / (2 * v_pi))
+#
+# # evaluate electric fields in splitter
+# splitter = Splitter(source.input_power, source.out_field)
+# splitter_fields = splitter.calculate_arms_input_fields()
+# # save p and q field for the 2 arms (I, Q)
+# x_field = splitter_fields[0]  # [mV/m]
+# y_field = splitter_fields[1]  # [mV/m]
+# splitter.A_in_field = x_field
+# splitter.B_in_field = y_field
 
-# evaluate electric fields in splitter
-splitter = Splitter(source.input_power, source.out_field)
-splitter_fields = splitter.calculate_arms_input_fields()
-# save p and q field for the 2 arms (I, Q)
-x_field = splitter_fields[0]  # [mV/m]
-y_field = splitter_fields[1]  # [mV/m]
-splitter.A_in_field = x_field
-splitter.B_in_field = y_field
-
-"""NB ARRIVATI A QUESTO PUNTO SIAMO AL PRIMO SPLITTER DOVE SI DIVIDONO I E Q"""
+"""NB ARRIVATI A QUESTO PUNTO SIAMO AL PRIMO SPLITTER DOVE SI DIVIDONO X E Y"""
 """ QUI VANNO INSERITI I VALORI DEI PRE-SOA"""
+pol_power_dbm = pre_soa_out_power()
+xi_power_dbm = pol_power_dbm - 3
+xq_power_dbm = xi_power_dbm
+
+xi_input_field = evaluate_field(xi_power_dbm)
+xq_input_field = evaluate_field(xq_power_dbm)
 
 # combiner output field
 
-combiner = Combiner(arm_a.out_field, arm_b.out_field)
-out_field_combiner = combiner.combiner_out_field(combiner.in_A, combiner.in_B)
-combiner.out_field = out_field_combiner
-# to check that combiner field is the same wrt eo_tf, try to take the abs**2 of combiner_out_field/sqrt()input_power)
-power_tf_check = (np.abs(combiner.out_field)/np.sqrt(input_power))**2
+# combiner = Combiner(arm_a.out_field, arm_b.out_field)
+# out_field_combiner = combiner.combiner_out_field(combiner.in_A, combiner.in_B)
+# combiner.out_field = out_field_combiner
+# # to check that combiner field is the same wrt eo_tf, try to take the abs**2 of combiner_out_field/sqrt()input_power)
+# power_tf_check = (np.abs(combiner.out_field)/np.sqrt(input_power))**2
 # create a list of Griffin MZMs using this input voltage interval
 # take into account the non-ideal params as b, c
 # for this evaluation, consider common mode voltage at 0
-griffin_mzm = InP_MZM(v_bias, v_A, v_B, gamma_1, gamma_2, phase_offset, b, c, er)
+
+# For MZi values, consider that MZ1 = YQ (0), MZ2 = YI (1), MZ3 = XQ (2), MZ4 = XI (3)
+vpi_phase = np.mean([v_pi_values[2], v_pi_values[3]])
+mz_xi = InP_MZM(lambda_wave, vcm_phase, vcm_bias, v_pi_values[3], v_diff_i, gamma_1, gamma_2, phase_offset, b, c)
+mz_xq = InP_MZM(lambda_wave, vcm_phase, vcm_bias, v_pi_values[2], v_diff_q, gamma_1, gamma_2, phase_offset, b, c)
+mz_phase = InP_MZM(lambda_wave, vcm_phase, vcm_bias, vpi_phase, v_diff_ph, gamma_1, gamma_2, phase_offset, b, c)
+
+# evaluate the electric fields of MZi at output
+mz_xi_field = mz_xi.griffin_eo_tf_field()*xi_input_field
+mz_xq_field = mz_xq.griffin_eo_tf_field()*xq_input_field
+
+mz_xi_power = field_to_power_dbm(mz_xi_field)
+mz_xq_power = field_to_power_dbm(mz_xq_field)
+
+# Apply the Phase Offset considering the vdiff_phase to be applied to the fields
+# At first evaluate the phases
+phases_shifts = griffin_phase_electrode(mz_phase.vl, mz_phase.vr, v_pi_values[3], v_pi_values[2])
+# Apply Phase Electrode phases independently to each XI and XQ arm
+mz_xi_ph_field = mz_xi_field * np.exp(phases_shifts[0] * 1j)
+mz_xq_ph_field = mz_xq_field * np.exp(phases_shifts[1] * 1j)
+
+mz_xi_ph_power = field_to_power_dbm(mz_xi_ph_field)     # [dBm]
+mz_xq_ph_power = field_to_power_dbm(mz_xq_ph_field)     # [dBm]
+"TODO: considera di trovare tramite software il valore da applicare al PHASE MODULATOR in modo " \
+"da ottenere una rotazione ideale"
+# combiner output field Griffin
+combiner_single_pol = Combiner(mz_xi_ph_field, mz_xq_ph_field)
+out_fields_combiner_single_pol = combiner_single_pol.combiner_out_field(combiner_single_pol.in_A,
+                                                                        combiner_single_pol.in_B)
+out_power_single_pol_pre_post_soa = field_to_power_dbm(out_fields_combiner_single_pol)      # [dBm]
+"TODO: QUI VA APPLICATA LA POTENZA DEL POST SOA"
+"FARE ATTENZIONE ALLE LOSS DEL POST SOA, FORSE VA TOLTA QUELLA DI QUAD LOSS PERCHE' GIA' IL COMBINER DA' LOSS"
+pout_post_soa = post_soa_out_power(out_power_single_pol_pre_post_soa)
+
+
 # evaluate Griffin V_bias to have the same V_pi of the classic MZM
 'Set a constant v_bias to get the same v_pi of the classic mzm, otherwise leave the variable one'
 v_bias = np.pi / v_pi